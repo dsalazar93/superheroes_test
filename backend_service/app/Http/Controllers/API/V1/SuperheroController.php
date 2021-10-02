e804e2de75290dfcfae0ef99b6fc2d3a632db888
dsalazar93 <salazar.gonzalez.d@gmail.com>
2021-10-02 18:51:35 -0500

    Endpoints iniciales para la entrega de los datos de los superheroes

--- backend_service/app/Http/Controllers/API/V1/SuperheroController.php
+++ backend_service/app/Http/Controllers/API/V1/SuperheroController.php
@@ -4,8 +4,39 @@ namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class SuperheroController extends Controller
 {
-    //
+    public function callApi(){
+        $response = Http::get('https://gateway.marvel.com/v1/public/characters',[
+            'ts' => '1',
+            'apikey' => 'edeb36852f8696cb83c487b2279bf494',
+            'hash' => '4f143c76c354fbfeead772ab3f62a179'
+        ]);
+
+        return $response->json();
+    }
+
+    public function callSuperHero($superhero_id){
+        $responseApi = $this->callApi();
+        $superHeroesData = $responseApi['data'];
+        
+        $superHeroPosition = array_search($superhero_id, array_column($superHeroesData['results'],'id'));
+        return $superHeroesData['results'][$superHeroPosition];
+    }
+
+    public function callSuperHeroes($superheroes_ids){
+        $responseApi = $this->callApi();
+        $superHeroesData = $responseApi['data'];
+
+        $ids = explode(",",$superheroes_ids);
+        $superHeroesRanking = [];
+        foreach($ids as $id){
+            $superHeroPosition = array_search($id, array_column($superHeroesData['results'],'id'));
+            array_push($superHeroesRanking, $superHeroesData['results'][$superHeroPosition]);
+        }
+
+        return $superHeroesRanking;
+    }
 }
--- backend_service/routes/api.php
+++ backend_service/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\V1\SuperheroController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('v1/allsuperheroes',[SuperheroController::class, 'callApi']);
+Route::get('v1/superhero/{superhero_id}', [SuperheroController::class, 'callSuperHero']);
+Route::get('v1/superheroes/{superheroes_ids}', [SuperheroController::class, 'callSuperHeroes']);
